38b9ca8ea5d425f0910b08b8af6dc3d2fbf47cc4
ZhangYuanze <62868277+PSIhlly@users.noreply.github.com>
2024-09-26 16:17:49 +0800

    指令发送机修改
    指令发送机修改

--- Assets/Z_Level1/Z_Client/Sample/Z_Client_Sample.cs
+++ Assets/Z_Level1/Z_Client/Sample/Z_Client_Sample.cs
@@ -3,62 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using Z_Client;
 
 public class Z_Client_Sample : MonoBehaviour
 {
     // Start is called before the first frame update
+    public InputField iF;
     void Start()
     {
-        ClientCore.Instance.Init(new Param[] { new Param( ProtoType.Tcp,1234, "127.0.0.1", 55555), new Param(ProtoType.Tcp, 5678, "127.0.0.1", 55555) });
+        ClientCore.Instance.Init(new Param[] { new Param( ProtoType.Tcp,1234, "118.31.13.94", 55550) });
     }
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+      
+      
+    }
+    public void Render()
+    {
+        System.Random random = new System.Random();
+        string code = "";
+        for(int i=0;i<5;i++)
         {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("1$weq"));
+            code += (char)random.Next('A', 'Z' + 1);
         }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("10$"));
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("11$1"));
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("12$"));
-        }
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("3$weq"));
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("13$0$1$1"));
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            ClientCore.Instance.Send(5678, Encoding.ASCII.GetBytes("1$sweq"));
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            ClientCore.Instance.Send(5678, Encoding.ASCII.GetBytes("10$"));
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            ClientCore.Instance.Send(5678, Encoding.ASCII.GetBytes("11$1"));
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            ClientCore.Instance.Send(5678, Encoding.ASCII.GetBytes("12$"));
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes("31$aaaaaaa"));
-        }
+        iF.text = code;
+    }
+    public void Send()
+    {
+        ClientCore.Instance.Send(1234, Encoding.ASCII.GetBytes(iF.text));
     }
 }

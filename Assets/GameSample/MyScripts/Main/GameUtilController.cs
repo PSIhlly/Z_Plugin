e9be3f2e7d94c04d34014abf730a013e74bc6c1d
ZhangYuanze <62868277+PSIhlly@users.noreply.github.com>
2025-04-13 21:09:20 +0800

    透明图
    透明图

--- Assets/GameSample/MyScripts/Main/GameUtilController.cs
+++ Assets/GameSample/MyScripts/Main/GameUtilController.cs
@@ -27,7 +27,7 @@ public class GameUtilController : Z_Controller<GameManager>
                     false,false,true
                 };
 
-        var res = CombineNewGoByPrefabs(name, new List<string>() { "Quad", "Quad", "Capsule" }, texRealName, new List<Vector3>() { Vector3.up * 0.3f, Vector3.up * 0.2f, Vector3.up*0.25f}, new List<Vector3>() { Vector3.one , Vector3.one, new Vector3(0.3f, 0.5f, 0.3f)  }, showShaddowLst);
+        var res = CombineNewGoByPrefabs(name, new List<string>() { "Quad", "Quad", "Capsule" }, texRealName, new List<Vector3>() { Vector3.up * 0.2f, Vector3.up * 0.1f, Vector3.up*0.25f}, new List<Vector3>() { Vector3.one , Vector3.one, new Vector3(0.3f, 0.5f, 0.3f)  }, showShaddowLst);
         if (forGame)
         {
             res.AddComponent<CharacterInstance>();
